23aef7bc496a2ee51518152d90d5e166ad8bd813
daylerjeff199906 <daylersan@gmail.com>
2024-06-16 09:50:53 -0500

    fixed table

--- src/app/(user)/(visor)/info-hidrologica/layout.tsx
+++ src/app/(user)/(visor)/info-hidrologica/layout.tsx
@@ -1,4 +1,14 @@
-import { FilterLayout, HidrologicalFilters } from '@/components'
+import dynamic from 'next/dynamic'
+
+const FilterLayout = dynamic(
+  () => import('@/components').then((mod) => mod.FilterLayout),
+  { ssr: false }
+)
+
+const HidrologicalFilters = dynamic(
+  () => import('@/components').then((mod) => mod.HidrologicalFilters),
+  { ssr: false }
+)
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- src/app/(user)/(visor)/info-meteorologica/layout.tsx
+++ src/app/(user)/(visor)/info-meteorologica/layout.tsx
@@ -1,5 +1,15 @@
 'use client'
-import { FilterLayout, MeteorologicalFilters } from '@/components'
+import dynamic from 'next/dynamic'
+
+const FilterLayout = dynamic(
+  () => import('@/components').then((mod) => mod.FilterLayout),
+  { ssr: false }
+)
+
+const MeteorologicalFilters = dynamic(
+  () => import('@/components').then((mod) => mod.MeteorologicalFilters),
+  { ssr: false }
+)
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
