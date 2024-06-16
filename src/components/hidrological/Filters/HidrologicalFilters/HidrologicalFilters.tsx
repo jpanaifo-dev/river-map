62d7fb6de9dacc9c5fc99acc4858faad3e20b70a
daylerjeff199906 <daylersan@gmail.com>
2024-06-15 21:54:47 -0500

    fixed layouts

--- src/components/hidrological/Filters/HidrologicalFilters/HidrologicalFilters.tsx
+++ src/components/hidrological/Filters/HidrologicalFilters/HidrologicalFilters.tsx
@@ -1,3 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
+import { useEffect } from 'react'
+// import { EstacionesData, UmbralData, ViewTypes } from './sections'
+import { useHidrological } from '@/hooks'
+
 export const HidrologicalFilters = () => {
-  return <></>
+  const { getHidroData, data, loading } = useHidrological()
+
+  useEffect(() => {
+    getHidroData()
+  }, [])
+
+  return (
+    <>
+      <main className="flex flex-col gap-3 w-full p-4">
+        <header>
+          <h1 className="font-bold">Facilíta tu búsqueda</h1>
+          <p className="text-xs text-gray-500">
+            Filtra la información específica que deseas obtener de las
+            estaciones hidrológicas
+          </p>
+        </header>
+        <article className="flex flex-col gap-2">
+          {/* <ViewTypes /> */}
+          {/* <EstacionesData
+            options={data?.Estacion}
+            loading={loading}
+          /> */}
+          {/* <UmbralData /> */}
+        </article>
+      </main>
+    </>
+  )
 }
--- src/components/layouts/FilterLayout.tsx
+++ src/components/layouts/FilterLayout.tsx
@@ -9,9 +9,9 @@ export const FilterLayout = (props: IProps) => {
   const { children, filters } = props
   return (
     <>
-      <main className="flex flex-col md:flex-row">
+      <main className="flex flex-col md:flex-row sm:pt-12">
         <aside className="w-full md:w-64 md:min-w-64 md:max-w-64 sticky top-0 h-screen">
-          <div className="bg-white p-4">{filters}</div>
+          <div className="bg-white">{filters}</div>
         </aside>
         <article className="w-full bg-white p-4">{children}</article>
       </main>
