bf7bb2fb92fdd7eac8037c254d788f3028ad948e
daylerjeff199906 <daylersan@gmail.com>
2024-06-15 21:29:52 -0500

    fixed layouts

--- src/components/layouts/FilterLayout.tsx
+++ src/components/layouts/FilterLayout.tsx
@@ -1,14 +1,17 @@
 'use client'
-import { usePathname } from 'next/navigation'
 
-export const FilterLayout = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname()
+interface IProps {
+  children: React.ReactNode
+  filters: React.ReactNode
+}
 
+export const FilterLayout = (props: IProps) => {
+  const { children, filters } = props
   return (
     <>
       <main className="flex flex-col md:flex-row">
         <aside className="w-full md:w-64 md:min-w-64 md:max-w-64 sticky top-0 h-screen">
-          {/* <FiltersSection /> */}
+          <div className="bg-white p-4">{filters}</div>
         </aside>
         <article className="w-full bg-white p-4">{children}</article>
       </main>
