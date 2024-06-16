ad5eb5aa64be8af91b132413bb2339d6563edb0b
daylerjeff199906 <daylersan@gmail.com>
2024-06-15 22:25:38 -0500

    fixed layout

---
+++ src/app/(user)/(visor)/info-hidrologica/layout.tsx
@@ -0,0 +1,9 @@
+import { FilterLayout, HidrologicalFilters } from '@/components'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <FilterLayout filters={<HidrologicalFilters />}>{children}</FilterLayout>
+    </>
+  )
+}
---
+++ src/app/(user)/(visor)/layout.tsx
@@ -0,0 +1,9 @@
+import { HidrologicalProvider } from '@/providers'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <HidrologicalProvider>{children}</HidrologicalProvider>
+    </>
+  )
+}
