5d7d4ac6c9c7ba12a5ce7698c6c8fd0ce8c4fca7
daylerjeff199906 <daylersan@gmail.com>
2024-06-15 21:14:00 -0500

    fixe layout page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './core'
 export * from './home'
+export * from './layouts'
+// export * from './ui'
---
+++ src/components/layouts/index.ts
@@ -0,0 +1,3 @@
+import { FilterLayout } from './FilterLayout'
+
+export { FilterLayout }
