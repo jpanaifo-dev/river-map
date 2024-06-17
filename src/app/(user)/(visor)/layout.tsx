1334764f8b485bb47cbdd66c5872cb4f23001190
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-06-17 10:04:01 -0500

    feat: Implement lazy loading for images in student and teacher cursos pages

--- src/app/(user)/(visor)/layout.tsx
+++ src/app/(user)/(visor)/layout.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { Suspense } from 'react'
-// import { HidrologicalProvider } from '@/providers'
 
 import dynamic from 'next/dynamic'
 const HidrologicalProvider = dynamic(() =>
