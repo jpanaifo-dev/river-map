2f8dad5e05b01649fcf04603658680e1bb1f114e
daylerjeff199906 <daylersan@gmail.com>
2024-06-22 10:12:12 -0500

    fixed aside

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,4 +1,5 @@
 import { NavBar } from './NavBar/NavBar'
 import { TableCustom } from './TableCustom/TableCustom'
+import { HeaderFilters } from './Filters/HeaderFilters'
 
-export { NavBar, TableCustom }
+export { NavBar, TableCustom, HeaderFilters }
--- src/components/hidrological/Filters/HidrologicalFilters/HidrologicalFilters.tsx
+++ src/components/hidrological/Filters/HidrologicalFilters/HidrologicalFilters.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react'
 import { useHidrological } from '@/hooks'
 import { EstacionDetails, EstacionesData } from './sections'
+import { HeaderFilters } from '@/components'
 
 export const HidrologicalFilters = () => {
   const { getHidroData, data, loading } = useHidrological()
@@ -14,13 +15,7 @@ export const HidrologicalFilters = () => {
   return (
     <>
       <main className="flex flex-col gap-3 w-full p-4">
-        <header>
-          <h1 className="font-bold text-white">Selección de estación</h1>
-          <p className="text-xs text-gray-300">
-            Para visualizar la información de la estación hidrológica,
-            seleccione una estación de la lista.
-          </p>
-        </header>
+        <HeaderFilters />
         <article className="flex flex-col gap-2">
           <EstacionesData
             options={data?.Estacion}
